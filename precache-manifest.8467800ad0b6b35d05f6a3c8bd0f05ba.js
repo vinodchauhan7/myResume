492a0294acb56039d23c5ac54211be37ad0d407d
Vinod Chauhan <vinodc45@gmail.com>
2019-10-16 22:52:10 +0530

    Updates

---
+++ precache-manifest.8467800ad0b6b35d05f6a3c8bd0f05ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a1777630297435b1e7aee718a992964",
+    "url": "/myResume/index.html"
+  },
+  {
+    "revision": "9f097d1e14394c663ef7",
+    "url": "/myResume/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ca6f319398fef351e533",
+    "url": "/myResume/static/css/main.4c48667a.chunk.css"
+  },
+  {
+    "revision": "9f097d1e14394c663ef7",
+    "url": "/myResume/static/js/2.68bb60b6.chunk.js"
+  },
+  {
+    "revision": "ca6f319398fef351e533",
+    "url": "/myResume/static/js/main.0498a5a1.chunk.js"
+  },
+  {
+    "revision": "8ca51c7836125d79971c",
+    "url": "/myResume/static/js/runtime-main.01fabbec.js"
+  }
+]);
